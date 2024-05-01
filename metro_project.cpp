e50102c2c9dd71912552c2b2057960f32dd535bd
Sahil-1234567 <168591704+Sahil-1234567@users.noreply.github.com>
2024-05-01 12:40:38 +0530

    Update metro_project.cpp

--- metro_project.cpp
+++ metro_project.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cstring> // For memset
 
-#include<bits/stdc++.h>
 
 #define ll long long int
 #define rep0(i,n)   for(int i=0;i<n;i++)
@@ -11,33 +14,33 @@ string metro_lst[V];
 int metro_no[V];
 
 void metro_fare(float dis){
-	cout<<"Expected Metro Fare (Weekdays except Sunday) would be: Rs. ";
-	if(dis<=2.0){ //expected metro fare for shortest route distance <= 2 km.
-		cout<<10.0<<endl;
-	}
-	else if(dis>2.0 && dis<=5.0){
-		cout<<20.0<<endl;
-	}
-	else if(dis>5.0 && dis<=12.0){
-		cout<<30.0<<endl;
-	}
-	else if(dis>12.0 && dis<=21.0){
-		cout<<40.0<<endl;
-	}
-	else if(dis>21.0 && dis<=32.0){
-		cout<<50.0<<endl;
-	}
-	else if(dis>32.0){
-		cout<<60.0<<endl;
-	}else{
-		cout<<"Not Known"<<endl;
-	}
-	cout<<endl;
+  cout<<"Expected Metro Fare (Weekdays except Sunday) would be: Rs. ";
+  if(dis<=2.0){ //expected metro fare for shortest route distance <= 2 km.
+    cout<<10.0<<endl;
+  }
+  else if(dis>2.0 && dis<=5.0){
+    cout<<20.0<<endl;
+  }
+  else if(dis>5.0 && dis<=12.0){
+    cout<<30.0<<endl;
+  }
+  else if(dis>12.0 && dis<=21.0){
+    cout<<40.0<<endl;
+  }
+  else if(dis>21.0 && dis<=32.0){
+    cout<<50.0<<endl;
+  }
+  else if(dis>32.0){
+    cout<<60.0<<endl;
+  }else{
+    cout<<"Not Known"<<endl;
+  }
+  cout<<endl;
 }
 
 int minDistance(float dist[],bool sptSet[]){
     float min = 1000.0;
-	int min_index;
+  int min_index;
 
     for (int v = 0; v < V; v++)
         if (sptSet[v] == false &&dist[v] <= min)
@@ -48,14 +51,14 @@ int minDistance(float dist[],bool sptSet[]){
 
 void printPath(int parent[], int j,int src,vector<int> &cnt){
     if (parent[j]==src || parent[j]<=0){
-    	if(parent[j]==src){
-    		cnt.push_back(metro_no[j]);
-			cout<<" -->>"<<metro_lst[metro_no[j]];
-		}
-		return;
-	}
+      if(parent[j]==src){
+        cnt.push_back(metro_no[j]);
+      cout<<" -->>"<<metro_lst[metro_no[j]];
+    }
+    return;
+  }
     printPath(parent, parent[j],src,cnt);
-	cnt.push_back(metro_no[j]);
+  cnt.push_back(metro_no[j]);
     cout<<" -->> "<<metro_lst[metro_no[j]];
 }
 
@@ -64,7 +67,7 @@ void printSolution(float dist[], int src,int parent[],int dest,vector<int> &cnt)
     metro_fare(dist[dest]);
     cout<<metro_lst[metro_no[src]];
     cnt.push_back(metro_no[src]);
-	printPath(parent, dest,src,cnt);
+  printPath(parent, dest,src,cnt);
 }
 
 void dijkstra(float graph[][V], int src, int d,vector<int> &cnt){
@@ -80,7 +83,7 @@ void dijkstra(float graph[][V], int src, int d,vector<int> &cnt){
 
     dist[src] = 0.0;
     for(int count = 0; count < V - 1; count++)
-	{
+  {
         int u = minDistance(dist, sptSet);
         sptSet[u] = true;
         for (int v = 0; v < V; v++)
@@ -132,242 +135,300 @@ float al[22]={0.0,0.3,1.3,1.0,1.1,0.9,2.0,1.1,1.5,1.6,1.0,1.4,1.2,1.7,1.5,1.6,1.
 void print_response()
 {
     cout<<endl<<endl<<endl<<endl;
-	cout<<"-------------------------------------------"<<endl;
-	cout<<"\t---   METRO APP   --- \t\t"<<endl;
-	cout<<"-------------------------------------------"<<endl;
-	cout<<"For finding shortest route,please enter 1. "<<endl;
-	cout<<"For Ticketing and Token information,please enter 2."<<endl;
-	cout<<"For Delhi Metro Stations,please enter 3."<<endl;
-	cout<<"For Exit, please enter any other key."<<endl;
-	cout<<"Enter Your Response: ";
+  cout<<"-------------------------------------------"<<endl;
+  cout<<"\t---   METRO APP   --- \t\t"<<endl;
+  cout<<"-------------------------------------------"<<endl;
+  cout<<"For finding shortest route,please enter 1. "<<endl;
+  cout<<"For Ticketing and Token information,please enter 2."<<endl;
+  cout<<"For Delhi Metro Stations,please enter 3."<<endl;
+  cout<<"For Exit, please enter any other key."<<endl;
+  cout<<"Enter Your Response: ";
 }
 
 void print_ticket(){
-	cout<<endl<<endl;
-	cout<<"----------TOKEN---------"<<endl;
-	cout<<"1. Only One way permitted"<<endl;
-	cout<<"2. Validity One day"<<endl;
-	cout<<"3. Refund with in 60 min"<<endl;
-	cout<<"4. Exit within 170 min, penalty Rs 10 per hour, subject to maximum Rs 50"<<endl;
+  cout<<endl<<endl;
+  cout<<"----------TOKEN---------"<<endl;
+  cout<<"1. Only One way permitted"<<endl;
+  cout<<"2. Validity One day"<<endl;
+  cout<<"3. Refund with in 60 min"<<endl;
+  cout<<"4. Exit within 170 min, penalty Rs 10 per hour, subject to maximum Rs 50"<<endl;
 
-	cout<<endl<<endl;
+  cout<<endl<<endl;
     cout<<"----------TRAVEL CARD---------"<<endl;
-	cout<<"1. First Time Rs 150 ( including Rs 50 refundable security"<<endl;
-	cout<<"2. Next recharge Rs 200 to Rs 2000"<<endl;
-	cout<<"3. Validity - 1 year after every recharge, max 10 years"<<endl;
-	cout<<"4. Same station entry/exit Rs 10 charged with in 20 min"<<endl;
-	cout<<endl<<endl;
+  cout<<"1. First Time Rs 150 ( including Rs 50 refundable security"<<endl;
+  cout<<"2. Next recharge Rs 200 to Rs 2000"<<endl;
+  cout<<"3. Validity - 1 year after every recharge, max 10 years"<<endl;
+  cout<<"4. Same station entry/exit Rs 10 charged with in 20 min"<<endl;
+  cout<<endl<<endl;
 
-	cout<<"----------BENIFITS OF DELHI METRO SMART CARD---------"<<endl;
-	cout<<"1. It provides 10 pre cent discount on every ride."<<endl;
-	cout<<"2. Off Peak hours - between 6am to 8am, 12pm to 5pm and 10 pm onwards - can avail a discount of 20 per cent."<<endl;
-	cout<<"3. On Sundays and National Holidays - there is a discount of Rs. 10"<<endl;
-	cout<<"4. You can buy card worth Rs 150 to Rs2000 at one time and return it anytime to get the balance amount back"<<endl;
-	cout<<"5. Refundable Security deposit Rs50"<<endl;
-	cout<<"6. Discount is not applicable if Entry/Exit is made from the same station"<<endl;
-	cout<<endl<<endl;
+  cout<<"----------BENIFITS OF DELHI METRO SMART CARD---------"<<endl;
+  cout<<"1. It provides 10 pre cent discount on every ride."<<endl;
+  cout<<"2. Off Peak hours - between 6am to 8am, 12pm to 5pm and 10 pm onwards - can avail a discount of 20 per cent."<<endl;
+  cout<<"3. On Sundays and National Holidays - there is a discount of Rs. 10"<<endl;
+  cout<<"4. You can buy card worth Rs 150 to Rs2000 at one time and return it anytime to get the balance amount back"<<endl;
+  cout<<"5. Refundable Security deposit Rs50"<<endl;
+  cout<<"6. Discount is not applicable if Entry/Exit is made from the same station"<<endl;
+  cout<<endl<<endl;
 
-	cout<<"----------TOURIST CARD---------"<<endl;
-	cout<<"1. Specially for a single day travel"<<endl;
-	cout<<"2. Purchase a card word Rs100 and travel whole day to any station on the Delhi Metro"<<endl;
-	cout<<"3. 1 day Rs 200 (Rs 150 + Rs 50 refundable security)"<<endl;
-	cout<<"4. 3 Days Rs 500 (Rs. 450 + Rs 50 refundable security)"<<endl;
-	cout<<endl<<endl;
+  cout<<"----------TOURIST CARD---------"<<endl;
+  cout<<"1. Specially for a single day travel"<<endl;
+  cout<<"2. Purchase a card word Rs100 and travel whole day to any station on the Delhi Metro"<<endl;
+  cout<<"3. 1 day Rs 200 (Rs 150 + Rs 50 refundable security)"<<endl;
+  cout<<"4. 3 Days Rs 500 (Rs. 450 + Rs 50 refundable security)"<<endl;
+  cout<<endl<<endl;
 
 }
 
 int main(){
-	map<string,int> metro;
+  map<string,int> metro;
 
-	float graph[V][V];
-	memset(graph,0.0,sizeof(graph));
-	int count=0;
+  float graph[V][V];
+  memset(graph,0.0,sizeof(graph));
+  int count=0,src,dt;
 
-	rep0(i,37){
-		if(!metro[yellow_line[i]]){
-			metro[yellow_line[i]]=count;
-			count++;
-		}
-	}
-	rep0(i,21){
-		if(!metro[red_line[i]]){
-			metro[red_line[i]]=count;
-			count++;
-		}
-	}
-	rep0(i,51){
-		if(!metro[blue_line[i]]){
-			metro[blue_line[i]]=count;
-			count++;
-		}
-	}
-	rep0(i,6){
-		if(!metro[orange_line[i]]){
-			metro[orange_line[i]]=count;
-			count++;
-		}
-	}
-	rep0(i,21){
-		if(!metro[green_line[i]]){
-			metro[green_line[i]]=count;
-			count++;
-		}
-	}
-	rep0(i,11){
-		if(!metro[rapid_line[i]]){
-			metro[rapid_line[i]]=count;
-			count++;
-		}
-	}
-	rep0(i,34){
-		if(!metro[voilet_line[i]]){
-			metro[voilet_line[i]]=count;
-			count++;
-		}
-	}
-	rep0(i,25){
-		if(!metro[magenta_line[i]]){
-			metro[magenta_line[i]]=count;
-			count++;
-		}
-	}
-	rep0(i,38){
-		if(!metro[pink_line[i]]){
-			metro[pink_line[i]]=count;
-			count++;
-		}
-	}
-	rep0(i,8){
-		if(!metro[blue_line2[i]]){
-			metro[blue_line2[i]]=count;
-			count++;
-		}
-	}
-	rep0(i,3){
-		if(!metro[green_line2[i]]){
-			metro[green_line2[i]]=count;
-			count++;
-		}
-	}
-	rep0(i,22){
-		if(!metro[aqua_line[i]]){
-			metro[aqua_line[i]]=count;
-			count++;
-		}
-	}
+  rep0(i,37){
+    if(!metro[yellow_line[i]]){
+      metro[yellow_line[i]]=count;
+      count++;
+    }
+  }
+  rep0(i,21){
+    if(!metro[red_line[i]]){
+      metro[red_line[i]]=count;
+      count++;
+    }
+  }
+  rep0(i,51){
+    if(!metro[blue_line[i]]){
+      metro[blue_line[i]]=count;
+      count++;
+    }
+  }
+  rep0(i,6){
+    if(!metro[orange_line[i]]){
+      metro[orange_line[i]]=count;
+      count++;
+    }
+  }
+  rep0(i,21){
+    if(!metro[green_line[i]]){
+      metro[green_line[i]]=count;
+      count++;
+    }
+  }
+  rep0(i,11){
+    if(!metro[rapid_line[i]]){
+      metro[rapid_line[i]]=count;
+      count++;
+    }
+  }
+  rep0(i,34){
+    if(!metro[voilet_line[i]]){
+      metro[voilet_line[i]]=count;
+      count++;
+    }
+  }
+  rep0(i,25){
+    if(!metro[magenta_line[i]]){
+      metro[magenta_line[i]]=count;
+      count++;
+    }
+  }
+  rep0(i,38){
+    if(!metro[pink_line[i]]){
+      metro[pink_line[i]]=count;
+      count++;
+    }
+  }
+  rep0(i,8){
+    if(!metro[blue_line2[i]]){
+      metro[blue_line2[i]]=count;
+      count++;
+    }
+  }
+  rep0(i,3){
+    if(!metro[green_line2[i]]){
+      metro[green_line2[i]]=count;
+      count++;
+    }
+  }
+  rep0(i,22){
+    if(!metro[aqua_line[i]]){
+      metro[aqua_line[i]]=count;
+      count++;
+    }
+  }
 
- 	rep0(i,36){
-  		int m=metro[yellow_line[i]];
-		int n=metro[yellow_line[i+1]];
-		graph[m][n]=yl[i+1];
-		graph[n][m]=yl[i+1];
-	}
-	rep0(i,20){
-  		int m=metro[red_line[i]];
-		int n=metro[red_line[i+1]];
-		graph[m][n]=rl[i+1];
-		graph[n][m]=rl[i+1];
-	}
-	rep0(i,50){
-  		int m=metro[blue_line[i]];
-		int n=metro[blue_line[i+1]];
-		graph[m][n]=bl[i+1];
-		graph[n][m]=bl[i+1];
-	}
-	rep0(i,5){
-  		int m=metro[orange_line[i]];
-		int n=metro[orange_line[i+1]];
-		graph[m][n]=ol[i+1];
-		graph[n][m]=ol[i+1];
-	}
-	rep0(i,20){
-  		int m=metro[green_line[i]];
-		int n=metro[green_line[i+1]];
-		graph[m][n]=gl[i+1];
-		graph[n][m]=gl[i+1];
-	}
-	rep0(i,10){
-  		int m=metro[rapid_line[i]];
-		int n=metro[rapid_line[i+1]];
-		graph[m][n]=rapl[i+1];
-		graph[n][m]=rapl[i+1];
-	}
-	rep0(i,33){
-  		int m=metro[voilet_line[i]];
-		int n=metro[voilet_line[i+1]];
-		graph[m][n]=vl[i+1];
-		graph[n][m]=vl[i+1];
-	}
-	rep0(i,24){
-  		int m=metro[magenta_line[i]];
-		int n=metro[magenta_line[i+1]];
-		graph[m][n]=ml[i+1];
-		graph[n][m]=ml[i+1];
-	}
-	rep0(i,37){
-  		int m=metro[pink_line[i]];
-		int n=metro[pink_line[i+1]];
-		graph[m][n]=pl[i+1];
-		graph[n][m]=pl[i+1];
-	}
-	rep0(i,7){
-  		int m=metro[blue_line2[i]];
-		int n=metro[blue_line2[i+1]];
-		graph[m][n]=bl2[i+1];
-		graph[n][m]=bl2[i+1];
-	}
-	rep0(i,2){
-  		int m=metro[green_line[i]];
-		int n=metro[green_line[i+1]];
-		graph[m][n]=gl2[i+1];
-		graph[n][m]=gl2[i+1];
-	}
-	rep0(i,21){
-  		int m=metro[aqua_line[i]];
-		int n=metro[aqua_line[i+1]];
-		graph[m][n]=al[i+1];
-		graph[n][m]=al[i+1];
-	}
-	int k=0;
-	for(auto i:metro){
-		metro_lst[k]=i.first;
-		metro_no[i.second]=k;
-		k++;
-	}
+  rep0(i,36){
+      int m=metro[yellow_line[i]];
+    int n=metro[yellow_line[i+1]];
+    graph[m][n]=yl[i+1];
+    graph[n][m]=yl[i+1];
+  }
+  rep0(i,20){
+      int m=metro[red_line[i]];
+    int n=metro[red_line[i+1]];
+    graph[m][n]=rl[i+1];
+    graph[n][m]=rl[i+1];
+  }
+  rep0(i,50){
+      int m=metro[blue_line[i]];
+    int n=metro[blue_line[i+1]];
+    graph[m][n]=bl[i+1];
+    graph[n][m]=bl[i+1];
+  }
+  rep0(i,5){
+      int m=metro[orange_line[i]];
+    int n=metro[orange_line[i+1]];
+    graph[m][n]=ol[i+1];
+    graph[n][m]=ol[i+1];
+  }
+  rep0(i,20){
+      int m=metro[green_line[i]];
+    int n=metro[green_line[i+1]];
+    graph[m][n]=gl[i+1];
+    graph[n][m]=gl[i+1];
+  }
+  rep0(i,10){
+      int m=metro[rapid_line[i]];
+    int n=metro[rapid_line[i+1]];
+    graph[m][n]=rapl[i+1];
+    graph[n][m]=rapl[i+1];
+  }
+  rep0(i,33){
+      int m=metro[voilet_line[i]];
+    int n=metro[voilet_line[i+1]];
+    graph[m][n]=vl[i+1];
+    graph[n][m]=vl[i+1];
+  }
+  rep0(i,24){
+      int m=metro[magenta_line[i]];
+    int n=metro[magenta_line[i+1]];
+    graph[m][n]=ml[i+1];
+    graph[n][m]=ml[i+1];
+  }
+  rep0(i,37){
+      int m=metro[pink_line[i]];
+    int n=metro[pink_line[i+1]];
+    graph[m][n]=pl[i+1];
+    graph[n][m]=pl[i+1];
+  }
+  rep0(i,7){
+      int m=metro[blue_line2[i]];
+    int n=metro[blue_line2[i+1]];
+    graph[m][n]=bl2[i+1];
+    graph[n][m]=bl2[i+1];
+  }
+  rep0(i,2){
+      int m=metro[green_line[i]];
+    int n=metro[green_line[i+1]];
+    graph[m][n]=gl2[i+1];
+    graph[n][m]=gl2[i+1];
+  }
+  rep0(i,21){
+      int m=metro[aqua_line[i]];
+    int n=metro[aqua_line[i+1]];
+    graph[m][n]=al[i+1];
+    graph[n][m]=al[i+1];
+  }
+  int k=0;
+  for(auto i:metro){
+    metro_lst[k]=i.first;
+    metro_no[i.second]=k;
+    k++;
+  }
 
-	int src;
-	int dt;
-	string response;
+  //int src;
+  //int dt;
+  string response;
 
-	print_response();
+  print_response();
 
-	cin>>response;
-	cout<<endl;
+  cin>>response;
+  cout<<endl;
 
-	if(response == "1"){
-		vector<int> cnt;
+  /*if (response == "1") {
+      vector<int> cnt;
 
-		cout<<"Enter starting station :";   cin>>src;
-		cout<<endl<<"Enter ending station :";   cin>>dt;
+      string src_station, dt_station;
 
-  		cout<<endl<<"Starting Station is "<<metro_lst[metro_no[src]]<<" and Ending Station is "<<metro_lst[metro_no[dt]]<<endl;
+      cout << "Enter starting station: ";   
+      cin.ignore(); // Ignore previous newline character
+      getline(cin, src_station);
 
-		dijkstra(graph,int(src),int(dt),cnt);
+      cout << endl << "Enter ending station: ";   
+      getline(cin, dt_station);
 
-		cout<<"\n\nNumber of station in between these two staions are : "<<cnt.size()<<endl;
-	}
-	else if(response == "2")
-	{
-	    print_ticket();
-	}
-	else if(response == "3")
-	{
-		for(auto i:metro){cout<<i.first<<"   |   "<<i.second<<endl;}
-	}
-	else
-	{
-	    return 0;
-	}
+      // Check if the entered stations exist in the metro map
+      if (metro.find(src_station) == metro.end() || metro.find(dt_station) == metro.end()) {
+          cout << "Invalid station name. Please enter valid station names." << endl;
+          return 1; // Exit with error code
+      }
 
-	return 0;
+      int src = metro[src_station];
+      int dt = metro[dt_station];
+
+      cout << endl << "Starting Station is " << src_station << " and Ending Station is " << dt_station << endl;
+
+      dijkstra(graph, src, dt, cnt);
+
+      cout << "\n\nNumber of stations in between these two stations are: " << cnt.size() << endl;
+  }
+  // -----------------------------------------------------
+  if(response == "1"){
+    vector<int> cnt;
+
+    cout<<"Enter starting station :";   cin>>src;
+    cout<<endl<<"Enter ending station :";   cin>>dt;
+
+      cout<<endl<<"Starting Station is "<<metro_lst[metro_no[src]]<<" and Ending Station is "<<metro_lst[metro_no[dt]]<<endl;
+
+    dijkstra(graph,int(src),int(dt),cnt);
+
+    cout<<"\n\nNumber of station in between these two staions are : "<<cnt.size()<<endl;
+  }*/
+  if (response == "1") {
+      vector<int> cnt;
+
+      string src_station, dt_station;
+
+      cout << "Enter starting station: ";   
+      cin.ignore(); // Ignore previous newline character
+      getline(cin, src_station);
+
+      cout << endl << "Enter ending station: ";   
+      getline(cin, dt_station);
+
+      // Check if the entered stations exist in the metro map
+      if (metro.find(src_station) == metro.end() || metro.find(dt_station) == metro.end()) {
+          cout << "Invalid station name. Please enter valid station names." << endl;
+          return 1; // Exit with error code
+      }
+
+      // Convert station names to station indices
+       src = metro[src_station];
+      dt = metro[dt_station];
+
+      cout << endl << "Starting Station is " << src_station << " and Ending Station is " << dt_station << endl;
+
+      // Now, src and dt are properly defined and can be used in dijkstra function
+      dijkstra(graph, src, dt, cnt);
+
+      cout << "\n\nNumber of stations in between these two stations are: " << cnt.size() << endl;
+  }
+
+  else if(response == "2")
+  {
+      print_ticket();
+  }
+  else if(response == "3")
+  {
+    for(auto i:metro){cout<<i.first<<"   |   "<<i.second<<endl;}
+  }
+  else
+  {
+      return 0;
+  }
+
+  return 0;
 }
